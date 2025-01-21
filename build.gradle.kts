9717c435db5b372fcda5e8687d2414fd3c8ea1a9
Romain Ilardi <romain.ilardi@gmail.com>
2025-01-21 14:31:34 +0100

    bug adb test 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    dependencies {
+        classpath("org.bouncycastle:bcutil-jdk18on:1.78.1")
+    }
+}
 plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
